648d5f77a72518585a5b6d077f2eefb4c9669c7e
lei23lei <lei23lei91@gmail.com>
2025-10-11 14:21:32 -0400

    Initial commit: Next.js app with dark mode toggle

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,25 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center px-6 h-12 bg-primary">
+      <Image
+        src="/images/ats-full.svg"
+        alt="Above The Spread"
+        width={100}
+        height={100}
+        className="w-48 mt-1 hidden md:block"
+      />
+      <Image
+        src="/images/ats.svg"
+        alt="Above The Spread"
+        width={100}
+        height={100}
+        className="w-14 mt-1 md:hidden"
+      />
+
+      <ThemeToggle />
+    </div>
+  );
+}
