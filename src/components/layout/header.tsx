96048c853daf50e75fc505188e68059b6381fb58
lei23lei <lei23lei91@gmail.com>
2025-10-12 15:06:46 -0400

    add link to icon back home

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,24 +1,29 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Header() {
   return (
     <div className="flex justify-between  items-center px-4 md:px-6 h-12 bg-primary">
-      <Image
-        src="/images/ats-full.svg"
-        alt="Above The Spread"
-        width={100}
-        height={100}
-        className="w-48 mt-1 hidden md:block"
-      />
-      <Image
-        src="/images/ats.svg"
-        alt="Above The Spread"
-        width={100}
-        height={100}
-        className="w-14 mt-1 md:hidden"
-      />
+      <Link href="/" className="cursor-pointer">
+        <Image
+          src="/images/ats-full.svg"
+          alt="Above The Spread"
+          width={100}
+          height={100}
+          className="w-48 mt-1 hidden md:block"
+        />
+      </Link>
+      <Link href="/" className="cursor-pointer">
+        <Image
+          src="/images/ats.svg"
+          alt="Above The Spread"
+          width={100}
+          height={100}
+          className="w-14 mt-1 md:hidden"
+        />
+      </Link>
 
       <div className="flex items-center gap-3">
         <ThemeToggle />
