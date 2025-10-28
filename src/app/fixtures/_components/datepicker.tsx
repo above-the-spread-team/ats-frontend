5daa9109a7e59eb0f298d1b625c888dc06c70503
lei23lei <lei23lei91@gmail.com>
2025-10-27 20:52:50 -0400

    change month bold

--- src/app/fixtures/_components/datepicker.tsx
+++ src/app/fixtures/_components/datepicker.tsx
@@ -278,7 +278,7 @@ export default function Datepicker() {
         ) : (
           <div className="w-12"></div>
         )}
-        <p className="text-md md:text-lg text-primary dark:text-mygray font-semibold ">
+        <p className="text-md md:text-lg text-primary dark:text-mygray font-bold ">
           {currentMonthYear}
         </p>
         <button
