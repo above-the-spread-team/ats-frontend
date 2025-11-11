a68a0183f0b0543da05a5900f2216b43e64a3954
lei23lei <lei23lei91@gmail.com>
2025-11-11 11:17:33 +0800

    center the loading full

--- src/components/common/loading-full.tsx
+++ src/components/common/loading-full.tsx
@@ -1,6 +1,6 @@
 export default function LoadingFull() {
   return (
-    <div className="flex h-[calc(100vh-150px)] items-center  justify-center p-4 w-full">
+    <div className="flex min-h-[60vh] w-full items-center justify-center p-4 md:h-[calc(100vh-150px)]">
       <div className="custom-loader"></div>
     </div>
   );
